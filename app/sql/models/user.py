c2a85502a25859a1a2a3e6764a37f0ebdb7250d6
praveen <praveen.balasundram@gmail.com>
2025-12-31 22:38:32 +0530

    (#17) Add created_at column to User

--- app/sql/models/user.py
+++ app/sql/models/user.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.sql.models.base import Base
@@ -8,3 +11,4 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
